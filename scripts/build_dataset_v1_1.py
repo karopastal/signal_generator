4e7cf22fc34553bc0cd1dd2deb75cd7aa609a066
karopastal <tpaskaro@gmail.com>
2020-09-27 01:12:51 +0300

    change python version sge

--- scripts/build_dataset_v1_1.py
+++ scripts/build_dataset_v1_1.py
@@ -1,5 +1,9 @@
-#!/gpfs0/kats/projects/Python-3.8.4/python
+#!/bin/python3
+
+#-------------------------------------------------#
+#    #!/gpfs0/kats/projects/Python-3.8.4/python   #
+#-------------------------------------------------#
 
 import os
 
-os.system('/gpfs0/kats/projects/Python-3.8.4/python -m src.analysis.datasets_factory.dataset_v1_1')
+os.system('/bin/python3 -m src.analysis.datasets_factory.dataset_v1_1')
--- scripts/build_dataset_v1_1.sh
+++ scripts/build_dataset_v1_1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export path_to_python="/gpfs0/kats/projects/Python-3.8.4/python"
+# export path_to_python="/gpfs0/kats/projects/Python-3.8.4/python"
+export path_to_python="/bin/python3"
 export script_path="/gpfs0/kats/users/talpas/projects/signal_generator/scripts/build_dataset_v1_1.py"
 
 qsub -cwd -q kats.q -S $path_to_python $script_path
