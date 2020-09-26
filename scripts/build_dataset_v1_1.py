5992d21b712a27d02b93a608971380a25ee2801a
karopastal <tpaskaro@gmail.com>
2020-09-27 01:01:33 +0300

    fix paths for sge

--- scripts/build_dataset_v1_1.py
+++ scripts/build_dataset_v1_1.py
@@ -1,6 +1,5 @@
 #!/gpfs0/kats/projects/Python-3.8.4/python
 
-print("test yay")
-# import os
-#
-# os.system('/gpfs0/kats/projects/Python-3.8.4/python -m src.analysis.datasets_factory.dataset_v1_1')
+import os
+
+os.system('/gpfs0/kats/projects/Python-3.8.4/python -m src.analysis.datasets_factory.dataset_v1_1')
