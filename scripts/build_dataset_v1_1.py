7a9da4292be8c9bae191ea5deb332e1689cafe2b
karopastal <tpaskaro@gmail.com>
2020-09-27 00:42:55 +0300

    tmp commit test build

--- scripts/build_dataset_v1_1.py
+++ scripts/build_dataset_v1_1.py
@@ -1,5 +1,6 @@
 #!/gpfs0/kats/projects/Python-3.8.4/python
 
-import os
-
-os.system('/gpfs0/kats/projects/Python-3.8.4/python -m src.analysis.datasets_factory.dataset_v1_1')
+print("test yay")
+# import os
+#
+# os.system('/gpfs0/kats/projects/Python-3.8.4/python -m src.analysis.datasets_factory.dataset_v1_1')
--- scripts/build_dataset_v1_1.sh
+++ scripts/build_dataset_v1_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-path_to_python="/gpfs0/kats/projects/Python-3.8.4/python"
-script_path="/gpfs0/kats/users/talpas/workbench/signal_generator/scripts/build_dataset_v1_1.py"
+export path_to_python="/gpfs0/kats/projects/Python-3.8.4/python"
+export script_path="/gpfs0/kats/users/talpas/projects/signal_generator/scripts/build_dataset_v1_1.py"
 
-qsub -cwd -q kats.q -S path_to_python $script_path
+qsub -cwd -q kats.q -S $path_to_python $script_path
