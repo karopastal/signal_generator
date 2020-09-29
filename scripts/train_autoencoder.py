931aed5c2ea3fccc1c6bf37da508675be6436bcb
karopastal <tpaskaro@gmail.com>
2020-09-29 16:18:35 +0300

    fix paths

--- scripts/train_autoencoder.py
+++ scripts/train_autoencoder.py
@@ -4,6 +4,6 @@ import os
 
 python_3_8_4 = '/gpfs0/kats/projects/Python-3.8.4/python'
 
-run_command = python_3_8_4 + ' -m src.analysis.analysis.models.create_pca_autoencoder'
+run_command = python_3_8_4 + ' -m src.analysis.models.create_pca_autoencoder'
 
 os.system(run_command)
