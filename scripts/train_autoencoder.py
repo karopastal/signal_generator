1c47b03ed7e381fec5a1d065098fe7389c2a08ad
karopastal <tpaskaro@gmail.com>
2020-09-30 16:03:32 +0300

    fix path

--- scripts/train_autoencoder.py
+++ scripts/train_autoencoder.py
@@ -5,7 +5,7 @@ import os
 python_3_8_4 = '/gpfs0/kats/projects/Python-3.8.4/python'
 
 train_pca_ae = python_3_8_4 + ' -m src.analysis.models.create_pca_autoencoder'
-train_deep_ae = python_3_8_4 + ' -m src.analysis.models.create_pca_autoencoder'
+train_deep_ae = python_3_8_4 + ' -m src.analysis.models.create_deep_ae_autoencoder'
 # train_conv_ae = python_3_8_4 + ' -m src.analysis.models.create_pca_autoencoder'
 # train_sparse_ae = python_3_8_4 + ' -m src.analysis.models.create_pca_autoencoder'
 # train_generative_ae = python_3_8_4 + ' -m src.analysis.models.create_pca_autoencoder'
