a47c7c008043d7911e888cd48bbad234bb8986c4
karopastal <tpaskaro@gmail.com>
2020-10-17 21:09:26 +0300

    train models

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,7 +1,35 @@
 from src.models.deep_ae import DeepAutoencoder
 
-path_dataset = 'data/dataset/Oct-16-20T14-39-16$25000'
 
-deep_ae = DeepAutoencoder(path_dataset=path_dataset, encoding_dim=8)
+def deep_ae_1():
+    encoding_dim = 8
+    path_dataset = 'data/dataset/Oct-16-20T14-39-16$25000'
+    deep_ae = DeepAutoencoder(path_dataset=path_dataset, encoding_dim=encoding_dim)
+    deep_ae.train_model(epochs=200, batch_size=64)
 
-deep_ae.train_model(epochs=5, batch_size=64)
+
+def deep_ae_2():
+    encoding_dim = 16
+    path_dataset = 'data/dataset/Oct-16-20T14-39-16$25000'
+    deep_ae = DeepAutoencoder(path_dataset=path_dataset, encoding_dim=encoding_dim)
+    deep_ae.train_model(epochs=200, batch_size=64)
+
+
+def deep_ae_3():
+    encoding_dim = 32
+    path_dataset = 'data/dataset/Oct-16-20T14-39-16$25000'
+    deep_ae = DeepAutoencoder(path_dataset=path_dataset, encoding_dim=encoding_dim)
+    deep_ae.train_model(epochs=200, batch_size=64)
+
+
+def main():
+    deep_ae_1()  # path =
+
+    """ to be trained """
+    # deep_ae_2() # path =
+    # deep_ae_3() # path =
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
